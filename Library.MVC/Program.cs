54a1cda688f218d379851c7d40be5a69c74315ca
Khaled El Hamzi <contact@elhamzi.me>
2022-02-22 13:15:55 +0100

    Delete & merge program and startup classes.

--- Library.MVC/Program.cs
+++ Library.MVC/Program.cs
@@ -1,43 +1,52 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Library.Application.Interfaces;
 using Library.Infrastructure.Persistence;
-using Microsoft.AspNetCore.Hosting;
+using Library.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Library.MVC
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//DbContext configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("LibrarySystem"),
+    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+);
+
+//Services configuration
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IBookCopyService, BookCopyService>();
+builder.Services.AddScoped<ILoanService, LoanService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IBookCopyLoanService, BookCopyLoanService>();
+
+builder.Services.AddRazorPages();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //// create the webHost 
-            //var webHost = CreateHostBuilder(args).Build();
-            //// create a method that runs our migrations and creates the database
-            //RunMigrations(webHost);
-            //// run the webHost
-            //webHost.Run();
-
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        //private static void RunMigrations(IHost webHost)
-        //{
-        //    using var scope = webHost.Services.CreateScope();
-        //    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        //    db.Database.Migrate();
-        //}
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseAuthentication();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
