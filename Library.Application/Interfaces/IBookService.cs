94ea2bc723df1c73a93fb5c5f9f1ce631f664a05
Khaled El Hamzi <contact@elhamzi.me>
2022-02-23 03:06:33 +0100

    Rewrite book interface service.

--- Library.Application/Interfaces/IBookService.cs
+++ Library.Application/Interfaces/IBookService.cs
@@ -1,48 +1,10 @@
 ﻿using Library.Domain;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq.Expressions;
 
 namespace Library.Application.Interfaces
 {
-    public interface IBookService
+    public interface IBookService : IAsyncGenericRepository<BookDetails>
     {
-        /// <summary>
-        /// Adds the book to DB
-        /// </summary>
-        /// <param name="book"></param>
-        void AddBook(BookDetails book);
-
-        /// <summary>
-        /// Updates a book
-        /// </summary>
-        /// <param name="book"></param>
-        void UpdateBookDetails(BookDetails book);
-
-         /// <summary>
-        /// Gets all books from the database
-        /// </summary>
-        /// <returns>list of books</returns>
-        ICollection<BookDetails> GetAllBooks();
-       
-        /// <summary>
-        /// Get only books that are not borrowed out
-        /// </summary>
-        /// <returns>A list of books</returns>
-        ICollection<BookDetails> GetAvailableBooks();
-
-        /// <summary>
-        /// Get a book from the database
-        /// </summary>
-        /// <param name="id">Returns book</param>
-        /// <returns></returns>
-        BookDetails FindBook(int id);
-
-        /// <summary>
-        /// Deletes the book from the database
-        /// </summary>
-        /// <param name="id"></param>
-        public void DeleteBookDetails(int id);
-
+        Task<BookDetails> GetBookOrDefaultAsync(Expression<Func<BookDetails, bool>> filter, string? includeProperties = null, bool tracked = true);
     }
 }
