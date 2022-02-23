4281f1d11f740fd70ac6ea6875bf5947ebbfddfe
Khaled El Hamzi <contact@elhamzi.me>
2022-02-23 02:25:30 +0100

    Add annotations to author model.

--- Library.Domain/Author.cs
+++ Library.Domain/Author.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Library.Domain
 {
     public class Author
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public IList<BookDetails> Books { get; set; }
+
+        [Display(Name="Full Name")]
+        [MaxLength(30)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public IList<BookDetails>? Books { get; set; }
     }
 }
