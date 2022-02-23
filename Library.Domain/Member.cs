633ca5018cd4229c96485352a74a3de89d614cfd
Khaled El Hamzi <contact@elhamzi.me>
2022-02-24 00:38:36 +0100

    Apply annotations to member properties.

--- Library.Domain/Member.cs
+++ Library.Domain/Member.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Domain
 {
     public class Member
     {
         public int Id { get; set; }
-        public string SSN { get; set; }
-        public string Name { get; set; }
+
+        [MaxLength(12)]
+        [Required]
+        public string SSN { get; set; } = "00000000-0000";
+
+        [Display(Name="Full Name")]
+        [MaxLength(30)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [ValidateNever]
         public IList<Loan> Loans { get; set; }
     }
 }
