a40a28c7aaba837b1df1b0e6f96b7b8076af08ca
Khaled El Hamzi <contact@elhamzi.me>
2022-02-23 02:20:22 +0100

    Add a generic interface repository.

---
+++ Library.Application/Interfaces/IAsyncGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Application.Interfaces
+{
+    public interface IAsyncGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(int id);
+    }
+}
