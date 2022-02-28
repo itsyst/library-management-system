a4c7a12b113382868b4c56ee3d75200d93ec408a
Khaled El Hamzi <contact@elhamzi.me>
2022-02-28 14:43:17 +0100

    Update member datatable js file.

--- Library.MVC/wwwroot/js/members.js
+++ Library.MVC/wwwroot/js/members.js
@@ -1,33 +1,33 @@
-﻿var table = $("#members")
+﻿$(document).ready(function () {
+    function Delete(url) {
+        Swal.fire({
+            title: `Are you sure you want to delete this member ?`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#5ea2bc',
+            confirmButtonText: 'Confirm'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: url,
+                    type: 'DELETE',
+                    success: function (data) {
+                        if (data) {
+                            if (data.success) {
+                                window.location.reload();
+                                toastr.success(data.message);
+                            }
+                            else {
+                                toastr.error(data.message);
+                            }
 
-function Delete(url) {
-    Swal.fire({
-        title: `Are you sure you want to delete this member ?`,
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#5ea2bc',
-        confirmButtonText: 'Confirm'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function (data) {
-                    if (data) {
-                        if (data.success) {
-                            window.location.reload();
-                            toastr.success(data.message);
                         }
-                        else {
-                            toastr.error(data.message);
-                        }
-
                     }
-                }
-            })
-        }
-    }).catch(() => {
-        toastr.error("An unexpected error occurred please try again later !");
-    })
-}
+                })
+            }
+        }).catch(() => {
+            toastr.error("An unexpected error occurred please try again later !");
+        })
+    }
+});
