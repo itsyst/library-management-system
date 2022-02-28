0fb3de9682972ce22649ab3871dded016eb82403
Khaled El Hamzi <contact@elhamzi.me>
2022-02-28 15:12:57 +0100

    Create book view model and delete rubbish files.

---
+++ Library.MVC/ViewModels/BookDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Library.Domain;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.MVC.ViewModels
+{
+    public class BookDetailsViewModel
+    {
+        public BookDetails? BookDetails { get; set; }
+        [ValidateNever]
+        [Display(Name = "Author")]
+        public IEnumerable<SelectListItem>? Authors { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? BookCopies { get; set; }
+        [ValidateNever]
+        public int Copies { get; set; } = 0;
+    }
+}
