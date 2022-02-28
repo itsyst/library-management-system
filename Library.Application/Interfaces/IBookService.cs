635dfed4097e0dbb26dd4504002426e98ac6b994
Khaled El Hamzi <contact@elhamzi.me>
2022-02-28 15:18:41 +0100

    Add new generic method to bookservice interface.

--- Library.Application/Interfaces/IBookService.cs
+++ Library.Application/Interfaces/IBookService.cs
@@ -5,6 +5,7 @@ namespace Library.Application.Interfaces
 {
     public interface IBookService : IAsyncGenericRepository<BookDetails>
     {
+        Task<IReadOnlyList<BookDetails>> GetAllBookDetailsAsync(Expression<Func<BookDetails, bool>>? filter = null, Func<IQueryable<BookDetails>, IOrderedQueryable<BookDetails>>? orderBy = null, params Expression<Func<BookDetails, object>>[] includeProperties);
         Task<BookDetails> GetBookOrDefaultAsync(Expression<Func<BookDetails, bool>> filter, string? includeProperties = null, bool tracked = true);
     }
 }
