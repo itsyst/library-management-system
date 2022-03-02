79dc36cc3cfc1428f519e23f14a6da76744c1ab9
Khaled El Hamzi <contact@elhamzi.me>
2022-03-02 22:37:08 +0100

    Fix bookcopy loan relationship model.

--- Library.Domain/BookCopyLoan.cs
+++ Library.Domain/BookCopyLoan.cs
@@ -7,10 +7,10 @@ namespace Library.Domain
     public class BookCopyLoan
     {
         public int BookCopyId { get; set; }
-        public BookCopy BookCopy{ get; set; }
+        public BookCopy? BookCopy{ get; set; }
 
         public int LoanId { get; set; }
-        public Loan Loan { get; set; }
+        public Loan? Loan { get; set; }
 
     }
 }
