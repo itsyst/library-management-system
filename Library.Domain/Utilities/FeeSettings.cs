f364466db569c83ae0982fc6cee529996dd850f1
Khaled El Hamzi <contact@elhamzi.me>
2022-03-02 22:42:45 +0100

    Create a fee class constant.

---
+++ Library.Domain/Utilities/FeeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Utilities
+{
+    public static class FeeSettings
+    {
+        public const double FeePerDayPerBook = 2;
+        public const double Days = 14;
+    }
+}
