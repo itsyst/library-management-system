a365433f2b777492fc439940bbb683eb0820b6c2
Khaled El Hamzi <contact@elhamzi.me>
2022-03-02 22:38:07 +0100

    Refactor code for loan model.

--- Library.Domain/Loan.cs
+++ Library.Domain/Loan.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
+﻿using Library.Domain.Utilities;
 
 namespace Library.Domain
 {
@@ -13,7 +8,7 @@ namespace Library.Domain
         public DateTime StartDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public DateTime DueDate { get; set; }
-        public int Fee { get; set; }
+        public double Fee { get; set; }
 
         public List<BookCopyLoan> BookCopyLoans { get; set; }
 
@@ -23,40 +18,14 @@ namespace Library.Domain
         public Loan()
         {
             StartDate = DateTime.Now;
-            DueDate = DateTime.Now.AddDays(14);
+            DueDate = DateTime.Now.AddDays(FeeSettings.Days);
         }
 
-        /// <summary>
-        /// Calculates the fees of the bookcopies that are overdue 
-        /// </summary>
-        /// 
-        public void CalculateFee(int nrBookcopies)
+        public double SetFee(int copies)
         {
-            var FeePerDayPerBook = 2;
-
-            //if (ReturnDate < StartDate)
-            //{
-            //int newFee;
-
-            if (DateTime.Now > DueDate)
-            {
-                Fee = (DateTime.Now - DueDate).Days * FeePerDayPerBook * nrBookcopies;
-            }
-            else
-                Fee = 0;
-
-            //Fee = newFee;
-            // Fee = (DateTime.Now > DueDate) ? (DateTime.Now - DueDate).Days * FeePerDayPerBook * nrBookcopies : 0;
-            //}
-            //else
-            //{
-           //     Fee = (ReturnDate > DueDate) ? (ReturnDate - DueDate).Days * FeePerDayPerBook * nrBookcopies : 0;
-            //}
+            return Fee = (DateTime.Now > DueDate) ? (DateTime.Now - DueDate).Days * (FeeSettings.FeePerDayPerBook) * copies : 0;
         }
 
-        /// <summary>
-        /// Sets the returndate 
-        /// </summary>
         public void SetReturnDate()
         {
             ReturnDate = DateTime.Now;
