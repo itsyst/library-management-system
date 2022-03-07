b8e3e071598661a315db93feb36474046d23781b
Khaled El Hamzi <contact@elhamzi.me>
2022-03-07 15:28:22 +0100

    Add new loan model view class.

---
+++ Library.MVC/ViewModels/LoanViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Library.Domain;
+using Library.Domain.Utilities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Library.MVC.ViewModels
+{
+    public class LoanViewModel
+    {
+
+        [ValidateNever]
+        public Loan? Loan { get; set; }
+
+        [ValidateNever]
+        public IReadOnlyList<Loan>? Loans { get; set; }
+
+
+        [ValidateNever]
+        public IReadOnlyList<BookCopy>? BookCopies { get; set; }
+
+        [ValidateNever]
+        public IReadOnlyList<BookCopyLoan>? BookCopyLoans { get; set; }
+
+
+
+
+        [ValidateNever]
+        public BookCopy? BookCopy { get; set; }       
+ 
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? Copies { get; set; }
+
+        [ValidateNever]
+        public Member? Member { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? Members { get; set; }
+    }
+}
