363cf357b3cfccd4ea4b4491bde2b6860ebbe766
Khaled El Hamzi <contact@elhamzi.me>
2022-03-07 15:22:24 +0100

    Create custom selectlistitem comparer.

---
+++ Library.Domain/Utilities/SelectListItemComparer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Library.Domain.Utilities
+{
+
+    public class SelectListItemComparer : IEqualityComparer<SelectListItem>
+    {
+        public bool Equals(SelectListItem x, SelectListItem y)
+        {
+            return x.Text == y.Text;
+        }
+
+        public int GetHashCode(SelectListItem item)
+        {
+            int hashText = item.Text == null ? 0 : item.Text.GetHashCode();
+            return hashText;
+        }
+    }
+
+}
