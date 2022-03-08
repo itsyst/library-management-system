98d4a49fe849e2eb945b5e1e770aacc4bdb00f75
Khaled El Hamzi <contact@elhamzi.me>
2022-03-08 14:27:08 +0100

    Update bookservice to fix exception.

--- Library.Infrastructure/Services/BookService.cs
+++ Library.Infrastructure/Services/BookService.cs
@@ -51,7 +51,7 @@ namespace Library.Infrastructure.Services
                     }
                 }
 
-                return await query.FirstAsync();
+                return query.FirstOrDefault();
             }
             else
             {
@@ -66,7 +66,7 @@ namespace Library.Infrastructure.Services
                     }
                 }
 
-                return await query.FirstAsync();
+                return query.FirstOrDefault();
             }
         }
     }
