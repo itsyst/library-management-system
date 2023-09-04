025d94494d76cbe84aea99373275cd59f89425d9
Khaled El Hamzi <khaled.elhamzi@nethouse.se>
2023-09-04 10:48:15 +0200

    Run migration & update database.

--- Library.Infrastructure/Migrations/20230904072522_Added ImageBinary column to bookdetails table..Designer.cs
+++ Library.Infrastructure/Migrations/20230904072522_Added ImageBinary column to bookdetails table..Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Library.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Library.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230904072522_Added ImageBinary column to bookdetails table.")]
+    partial class AddedImageBinarycolumntobookdetailstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
---
+++ Library.Infrastructure/Migrations/20230904072522_Added ImageBinary column to bookdetails table..cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedImageBinarycolumntobookdetailstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageBinary",
+                table: "BookDetails",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 8,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 9,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 11,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 12,
+                column: "ImageBinary",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 13,
+                column: "ImageBinary",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageBinary",
+                table: "BookDetails");
+        }
+    }
+}
