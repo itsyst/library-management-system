00c492d4f4a00b7bc77db0617e216f593d3f328b
Khaled El Hamzi <contact@elhamzi.me>
2025-07-16 23:09:39 +0200

    feat(db): add new migrations and update schema

--- Library.Infrastructure/Migrations/20250711184341_AddUsefulPropertiesAndUpdateEntities.Designer.cs
+++ Library.Infrastructure/Migrations/20250711184341_AddUsefulPropertiesAndUpdateEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Library.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Library.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250711184341_AddUsefulPropertiesAndUpdateEntities")]
+    partial class AddUsefulPropertiesAndUpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
---
+++ Library.Infrastructure/Migrations/20250711184341_AddUsefulPropertiesAndUpdateEntities.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsefulPropertiesAndUpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 11, 18, 43, 40, 585, DateTimeKind.Utc).AddTicks(5378));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookCopies",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "BookDetails",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 7, 7, 20, 58, 4, 239, DateTimeKind.Utc).AddTicks(8072));
+        }
+    }
+}
