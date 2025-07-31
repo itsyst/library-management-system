ef2294523a737041263f48f989d4612801a734dc
Khaled El Hamzi <contact@elhamzi.me>
2025-07-31 16:28:35 +0200

    style: enhance CSS variables and UI shadows and remove redundant Js files.
    - update shadow variables with layered rgba shadows for better depth
    - refine SweetAlert2 popup styling and button positioning

--- Library.MVC/wwwroot/lib/jquery-validation/dist/jquery.validate.js
+++ Library.MVC/wwwroot/lib/jquery-validation/dist/jquery.validate.js
@@ -794,9 +794,6 @@ $.extend( $.validator, {
 						return false;
 					}
 				} catch ( e ) {
-					if ( this.settings.debug && window.console ) {
-						console.log( "Exception occurred when checking element " + element.id + ", check the '" + rule.method + "' method.", e );
-					}
 					if ( e instanceof TypeError ) {
 						e.message += ".  Exception occurred when checking element " + element.id + ", check the '" + rule.method + "' method.";
 					}
